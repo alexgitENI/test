a6903edf2e69b4bc103eb68434a414815165f31d
alexgitENI <alexgalaxy67@gmail.com>
2022-05-23 14:43:16 +0200

    test 2

--- src/main/java/ClassTest.java
+++ src/main/java/ClassTest.java
@@ -4,6 +4,12 @@ public class ClassTest {
 	public static void main(String[] args) {
 		System.out.println("coucou");
 		System.out.println("coucou c'est moi");
+		
+		System.out.println("coucou");
+		System.out.println("coucou c'est moi");
+		
+		
+	
 	}
 
 }
